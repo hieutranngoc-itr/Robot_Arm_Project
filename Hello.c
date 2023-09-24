64fb7863779f2bb35cfd9535f09db6c598fe3c9b
Thach Vu <thach6656@gmail.com>
2023-09-24 11:42:46 +0700

    update: added new code

--- Hello.c
+++ Hello.c
@@ -1,8 +1,20 @@
 #include "stdio.h"
 
 int main(void){
-
-    printf("Canh tay");
-
+    int *ptr;
+    int arr[5] = {};
+    for (int j = 0; j < 5; j++){
+        arr[j] = j;
+    }
+    int a = 2;
+    ptr = &arr[1];
+    printf("ptr address: %d\n", ptr);
+    printf("ptr value: %d\n", *ptr);
+    int i = 0;
+    /*
+    for(i = 0; i < 5; i++){
+        printf("i = %d\n", &i);
+    }
+    */
     return 0;
 }
